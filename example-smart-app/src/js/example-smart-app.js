194b787f54624cd379bf6a20e7e6a658d0ba0163
Eric Wilson <eric.a.wilson@cerner.com>
2018-04-25 13:01:13 -0500

    💩 Add console.log statement for checking purposes.

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -24,7 +24,8 @@
 
         $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv).done(function(patient, obv)
+          console.log('🐨', obv);
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
